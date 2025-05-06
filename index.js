fcd1a550c19fdef994dad8bfd2d108a596dba352
DevendraCodes93 <devu935352@gmail.com>
2025-05-06 23:31:36 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 /**/
 const app = express();
 app.use(cookieParser());
-app.use(morgan("dev"));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // https://socialdev1.netlify.app
