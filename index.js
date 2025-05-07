7df49ca5ff84c0e2d0291e2cff666d4f04b294e8
devendra hosamani <devu935352@gmail.com>
2025-05-07 09:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-// https://socialdev1.netlify.app
+// https://reelxplore93.web.app
 app.use(cors({ origin: " https://socialdev1.netlify.app", credentials: true }));
 dbConnect();
 app.use((req, res, next) => {
