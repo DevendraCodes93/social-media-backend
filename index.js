3576121ad23cf53be4e22a08a4ac02f0a9d787b3
DevendraCodes93 <devu935352@gmail.com>
2025-05-10 14:16:53 +0530

    new

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ dotenv.config();
 /**/
 const app = express();
 app.use(cookieParser());
-app.use(
-  express.static("public", {
-    maxAge: "1y", // or 31536000000 ms
-    setHeaders: (res, path) => {
-      res.setHeader("Cache-Control", "public, max-age=31536000");
-    },
-  })
-);
+// app.use(
+//   express.static("public", {
+//     maxAge: "1y", // or 31536000000 ms
+//     setHeaders: (res, path) => {
+//       res.setHeader("Cache-Control", "public, max-age=31536000");
+//     },
+//   })
+// );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // https://socialdev1.netlify.app
