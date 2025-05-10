7cca91d676c0401ccc769aec3a0ee3aa9e4c1292
DevendraCodes93 <devu935352@gmail.com>
2025-05-10 14:15:16 +0530

    latest version

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // https://socialdev1.netlify.app
-app.use(cors({ origin: " https://socialdev1.netlify.app", credentials: true }));
+
+app.use(cors({ origin: " https://reelxplore93.web.app", credentials: true }));
+
 dbConnect();
 app.use((req, res, next) => {
   res.setTimeout(500000, () => {
